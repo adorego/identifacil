bd79f323d5f20149f56ce86bf890a8e79f047348
Andres Do Rego Barros <adoregoel@gmail.com>
2024-01-29 10:00:09 -0300

    Con correccion de variable de entorno de url de registro

--- src/components/registro/FormRegister.tsx
+++ src/components/registro/FormRegister.tsx
@@ -86,7 +86,7 @@ export default function FormRegister(){
         
         contadorReconocimiento.current = 0;
         //Sin Kubernetes
-        const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/registro/`;
+        const url = `${process.env.NEXT_PUBLIC_IDENTIFACIL_IDENTIFICACION_REGISTRO_API}/api/registro/`;
         console.log('url:', url);
         setProgresoRegistro(EstadosProgreso[2]);
         const result = await fetch(url,{
