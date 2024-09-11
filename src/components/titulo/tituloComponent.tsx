f09f78a92e3aefe42dd7e66fc69b798061c683e0
andres <adoregoel@gmail.com>
2024-09-11 16:34:10 -0400

    Correxión de bug en tituloComponente

--- src/components/titulo/tituloComponent.tsx
+++ src/components/titulo/tituloComponent.tsx
@@ -21,8 +21,8 @@ export default function TituloComponent({titulo, url = "", newEntry = "", childr
 
     useEffect(() => {
         console.log('Check URL: ', url)
-        console.log('check includes ori: ', url.includes("/crear" && url))
-        console.log('check includes new: ', url.includes("/crear"))
+    //    console.log('check includes ori: ', url.includes("/crear" && url))
+    //    console.log('check includes new: ', url.includes("/crear"))
         if (!url.includes("/crear") && url !== '') {
             console.log('check entro')
 
